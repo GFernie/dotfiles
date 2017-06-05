03eff244d2c56bfea52d2450154b462c34cd1932
Gary Fernie <Gary.Fernie@skyscanner.net>
2017-06-05 23:21:37 +0100

    Add install script

---
+++ link_dotfiles.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Link dotfiles from this repo to their appropriate locations.
+
+set -e
+
+cd dotfiles
+dotfiles=$(find . -type f)
+
+for dotfile in ${dotfiles[@]}
+do
+    dotfile=$(echo $dotfile | cut -c 2-)
+    dotfile_src=$(pwd)$dotfile
+    dotfile_dst=${dotfile/#\/\~/$HOME}
+    mkdir -p $(dirname $dotfile_dst)
+    rm -f $dotfile_dst
+    echo "ln -sf $dotfile_src $dotfile_dst"
+    ln -sf $dotfile_src $dotfile_dst
+done
